c794b75ea128636dc60678c123ad3627a35f31dd
우정환 <woojja@msn.com>
2017-10-08 21:16:20 +0900

    Source Upload

---
+++ Module/MainModuleLibrary.ts
@@ -0,0 +1,14 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {CustomerComponent} from "../Component/CustomerComponent";
+
+@NgModule({
+    imports : [BrowserModule, FormsModule],
+    declarations : [CustomerComponent],
+    bootstrap : [CustomerComponent]
+})
+
+export class MainModuleLibrary{
+
+}
